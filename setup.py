198b310e65f9139c86a61d5c232c999f472a4665
unneyon <debik.39@yandex.ru>
2025-01-10 12:09:34 +0100

    update setup

--- setup.py
+++ setup.py
@@ -1,28 +1,28 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
-with open('README.md') as readme:
+with open("README.md") as readme:
     long_description = readme.read()
 
 
 setup(
-    name='netschoolapi',
-    version='11.0.5',
-    description='Асинхронный API-клиент для «Сетевого города»',
+    name="netschoolapi",
+    version="11.0.6",
+    description="Асинхронный API-клиент для «Сетевого города»",
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    author='nm17',
-    author_email='dannevergame@gmail.com',
-    url='https://github.com/nm17/netschoolapi/',
-    packages=['netschoolapi'],
-    package_data={'netschoolapi': ['py.typed']},
+    long_description_content_type="text/markdown",
+    author="unneyon",
+    author_email="me@unneyon.ru",
+    url="https://github.com/unneyon/netschoolapi/",
+    packages=find_packages(),
+    package_data={"netschoolapi": ["py.typed"]},
     classifiers=[
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: Russian',
-        'Topic :: Communications :: Chat',
-        'Topic :: Education',
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: Russian",
+        "Topic :: Communications :: Chat",
+        "Topic :: Education",
     ],
-    license='MIT',
+    license="MIT",
     install_requires=open("requirements.txt").read().strip().split("\n"),
-    python_requires='>=3.7',
+    python_requires=">=3.8",
 )
